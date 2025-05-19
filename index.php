fdbc86b30b948f3a5a36720578cf271b447993f9
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-05-19 11:44:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
+<html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Escape Room</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>Welkom</h1>
+
+
+  <button><a href="room_1.php">Klik hier voor een demonstratie van kamer 1</a></button>
+
+</body>
 
 </html>
 <?php
