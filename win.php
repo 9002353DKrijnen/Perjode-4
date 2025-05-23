944323bcdbbcbb068fce4c3861cfa39433f0de2a
AHMAD <9021811@student.zadkine.nl>
2025-05-23 08:42:13 +0200

    Create win.php

---
+++ win.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Je bent ontsnapt!</title>
+</head>
+<body style="background-color: green; color: white; text-align: center;">
+    <h1>🎉 Gefeliciteerd!</h1>
+    <p>Je hebt alle vragen goed beantwoord en bent op tijd ontsnapt!</p>
+    <a href="index.php" style="color: white;">Terug naar home</a>
+</body>
+</html>
