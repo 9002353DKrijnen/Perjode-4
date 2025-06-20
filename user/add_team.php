9681002ad0f2ee70e433d5d5745f0ebe293527d7
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-20 13:05:28 +0200

    Update add_team.php

--- user/add_team.php
+++ user/add_team.php
@@ -26,13 +26,21 @@ $statement = $conn->prepare($sqlQuery);
 // execute query
 $statement->execute();
 
-$statement->fetchAll();
+
+// You want to have the result fetched as an array, so we can use foreach to have a new line in the table for each record 
+$result = $statement->fetchAll();
+
+// foreach loop for result (input)
+echo "<select id='username'>";
+foreach ($result as $user) {
+    // instead of string concatenation we use string interpolation, because I find it much easier to read. 
+echo "<option value='{$user['id']}'>{$user['username']} </option>";
+}
 
 ?>
 <input type="submit" value="Team toevoegen">
 
 </form>
-
 
 
 
