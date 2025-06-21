806ebf075432774cab26b8cbaa58ca15ca54065f
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-21 17:01:22 +0200

    Opstart edit_teams.php

--- admin/edit_team.php
+++ admin/edit_team.php
@@ -1,5 +1,24 @@
 <?php
 /* Als admin wil ik teams kunnen bekijken, aanpassen of verwijderen zodat ik het overzicht kan behouden. (Student Damiën)*/
 
+// checkif there's an active session right now
+if(PHP_SESSION_NONE == session_status()) {
+
+    // start seession
+    session_start();
+}
+
+
+// if the username does not have the function is Admin then it will be redirected to the home page
+if(!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] === false) {
+    header("Location: ../index.php");
+    exit;
+}
+
+
+// database import
+include '../dbcon.php';
+
+
 
 ?>
