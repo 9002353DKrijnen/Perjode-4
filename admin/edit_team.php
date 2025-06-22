bc1b6b7df8e2b6a0a59b33cda87de78c7e3cc715
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-22 22:49:23 +0200

    Update edit_team.php

--- admin/edit_team.php
+++ admin/edit_team.php
@@ -2,7 +2,7 @@
 /* Als admin wil ik teams kunnen bekijken, aanpassen of verwijderen zodat ik het overzicht kan behouden. (Student Damiën)*/
 
 // checkif there's an active session right now
-if(PHP_SESSION_NONE == session_status()) {
+if (PHP_SESSION_NONE == session_status()) {
 
     // start seession
     session_start();
@@ -10,7 +10,7 @@ if(PHP_SESSION_NONE == session_status()) {
 
 
 // if the username does not have the function is Admin then it will be redirected to the home page
-if(!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] === false) {
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] === false) {
     header("Location: ../index.php");
     exit;
 }
@@ -19,6 +19,46 @@ if(!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] === fals
 // database import
 include '../dbcon.php';
 
-
-
+/* this page is admin-only. This means that only the admin can access this page, in order to edit,view or delete teams.
+We will begin by creating an sql command. But we will first make a regular html page to get the data.  */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teams</title>
+</head>
+
+<body>
+    <?php
+
+    // sqlQuery
+    $sqlQuery = "SELECT teamnaam, GROUP_CONCAT(username ORDER BY id SEPARATOR ' - ')
+     AS spelers FROM teams GROUP BY TeamID, teamnaam";
+    // prepare statement
+    $statement = $conn->prepare($sqlQuery);
+
+    // run the query
+    $statement->execute();
+
+    // fetch in associative array
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+    // table with teams
+
+    echo "<table>";
+    echo "<thead border='1' cellpadding='10'><tr><th>Teamnaam</th><th>Spelers</th></tr></thead>";
+    foreach ($result as $team) {
+        echo "<tr>";
+        echo "<td>{$team['teamnaam']} </td>";
+        echo "<td>- {$team['spelers']}</td>";
+        echo "</tr>";
+    }
+
+    ?>
+</body>
+
+</html>
