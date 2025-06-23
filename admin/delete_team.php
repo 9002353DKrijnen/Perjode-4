0b45c57d3f59cb7330696bfb6130f0e73ba86641
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-23 15:36:55 +0200

    delete.php

---
+++ admin/delete_team.php
@@ -0,0 +1,12 @@
+<?php
+if(isset($_POST['TeamID'])){
+    $TeamID = $_POST['TeamID'];
+    $sql = "DELETE FROM teams WHERE TeamID = :TeamID";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':TeamID', $TeamID);
+    $stmt->execute();
+    header("Location: edit_team.php");
+    exit;
+}
+
+?>
