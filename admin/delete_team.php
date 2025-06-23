ce1db0d4628b9498479012f2215ac3214848b87a
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-23 16:30:24 +0200

    delete is gereed

--- admin/delete_team.php
+++ admin/delete_team.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_POST['TeamID'])){
+    include_once '../dbcon.php';
     $TeamID = $_POST['TeamID'];
     $sql = "DELETE FROM teams WHERE TeamID = :TeamID";
     $stmt = $conn->prepare($sql);
--- admin/edit_team.php
+++ admin/edit_team.php
@@ -64,12 +64,53 @@ We will begin by creating an sql command. But we will first make a regular html 
         echo "<td>- {$team['spelers']}</td>";
 
         // edit and delete buttons
-        echo "<td><a href='edit_team.php?team_id={$team['TeamID']}'>Bewerken</a></td>";
-        echo "<td><a href='delete_team.php?team_id={$team['TeamID']}'>Verwijderen</a></td>";
+        echo "<td><a href='#' class='post-link' data-action='edit_team.php' data-id='{$team['TeamID']}'>Bewerken</a></td>";
+        echo "<td><a href='#' class='post-link' data-action='delete_team.php' data-id='{$team['TeamID']}'>Verwijderen</a></td>";
+
         echo "</tr>";
     }
     echo "</table>";
     ?>
+    <!-- hidden form toe make it easier to handle the post request done by the edit_team.php page -->
+    <form method="post" id="postForm" style="display: none">
+        <input type="hidden" name="TeamID" id="hiddenTeamID">
+    </form>
+    <script>
+        // if content is loaded load Eventlistener to the page
+        document.addEventListener('DOMContentLoaded', () => {
+            // import form and input
+            const form = document.getElementById('postForm');
+            const input = document.getElementById('hiddenTeamID');
+
+            // for each link we want to prevent to load to a random place (#), we will use preventDefault to prevent that default action and instead let javascript handle the inputted value
+
+            document.querySelectorAll('.post-link').forEach(link =>{
+                link.addEventListener('click',function(event){
+                    // prevent default #
+                    event.preventDefault();
+
+                    // get the given 
+                    const action = this.dataset.action;
+                    const teamID = this.dataset.id;
+
+                    //prevent accidental deletion 
+                    if(action.includes('delete')){
+                        const isConfirmed = confirm(`Weet u zeker dat u het team met teamid ${teamID} wilt verwijderen?`)
+                        if(!isConfirmed) return;
+                    }
+                    //input value is team id 
+                    input.value = teamID;
+                    // delete or edit 
+                    form.action = action;
+                    
+                    // submit
+                    form.submit();
+                })
+            })
+
+
+        })
+    </script>
 </body>
 
 </html>
