8655520c49c8b73abbd9252f6bc8f7db77e3157e
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-24 10:56:08 +0200

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,23 +1,6 @@
 <?php
 session_start();
-
-// Vernietig alle sessiegegevens
-$_SESSION = [];
-
-// Verwijder het sessie-cookie
-if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(),
-        '',
-        time() - 42000,
-        $params['path'],
-        $params['domain'],
-        $params['secure'],
-        $params['httponly']
-    );
-}
-
+session_unset();
 session_destroy();
 
 // Doorsturen naar de HOOFD-index (niet admin/index.php)
